1754a498ce71ba072d7c28fb485908df29eecb7a
George Paton <georgewpaton@gmail.com>
2017-07-22 23:52:48 +1000

    Add Tumblr AJAX calls

--- src/scripts/theme.js
+++ src/scripts/theme.js
@@ -1,3 +1,21 @@
 (function () {
-    
+    /**
+     * @desc This will be triggered when a new page of Notes is loaded and ready to be inserted.
+     * @param {Element} html - The HTML node that the AJAX notes loading function returns.
+     * @returns {Boolean}    - If this function returns false, it will block the Notes from being inserted.
+     */
+    var onNotesLoaded = function (html) {
+        return true;
+    };
+
+    /**
+     * @desc This will be triggered after a new page of Notes has been inserted into the DOM.
+     */
+    var onNotesInserted = function () {
+
+    };
+
+    // Expose the note insertion functions
+    window.tumblrNotesLoaded = onNotesLoaded;
+    window.tumblrNotesInserted = onNotesInserted;
 })();
